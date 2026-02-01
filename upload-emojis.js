81876ff400c027dff9f5caac71fd0f55fbef0f85
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2026-01-31 20:39:41 -0600

    Fix uploading emojis

--- upload-emojis.js
+++ upload-emojis.js
@@ -2,57 +2,56 @@ const Discord = require('discord.js');
 const fs = require('fs').promises;
 const path = require('path');
 const readline = require('readline');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
 const helper = require('./helper.js');
 
-const client = new Discord.Client({autoReconnect:true});
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildEmojisAndStickers
+  ]
+});
 
 client.on('error', console.error);
-
 const config = require('./config.json');
-
 let guilds = [];
 
 client.on('ready', () => {
-    [...client.guilds.cache].forEach(guild => {
-        if(guild.me.hasPermission('MANAGE_EMOJIS'))
+    client.guilds.cache.forEach(guild => {
+        if(guild.members.me.permissions.has(Discord.PermissionFlagsBits.ManageEmojisAndStickers))
             guilds.push(guild);
     });
-
+    
     if(guilds.length == 0)
         throw "Bot has no servers to upload emotes to";
-
+    
     guilds.forEach((guild, index) => {
         let staticEmojis = guild.emojis.cache.filter(a => !a.animated && !a.deleted && !a.managed).size;
         console.log(index, `${guild.name} - ${staticEmojis} / 50 or more emote slots`);
     });
-
+    
     rl.question('Server to upload the emotes to (server index): ', answer => {
         let index = Number(answer);
-
         if(isNaN(index))
             throw "Input is not a number";
-
         if(index < 0 || index >= guilds.length)
             throw "Invalid range, please choose a valid server index";
-
+        
         let guild = guilds[index];
-
         fs.readdir('./emotes').then(files => {
             let promises = [];
-
             files.forEach(file => {
                 if(path.extname(file) == '.png')
                     promises.push(
-                        guild.emojis.create(`./emotes/${file}`, path.basename(file, path.extname(file)))
+                        guild.emojis.create({
+                            attachment: `./emotes/${file}`,
+                            name: path.basename(file, path.extname(file))
+                        })
                     );
             });
-
             Promise.all(promises).then(() => {
                 console.log(`${promises.length} emotes successfully uploaded!`);
                 rl.close();
